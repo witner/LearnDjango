1e8a9f06c09f4d3630b85558486b7b6ba141c8c0
huangdongdong <huangdongdong@daojia-inc.com>
2019-12-06 11:25:41 +0800

    A-04 Django-第一部分（快速入门）-快速搭建项目的模型

--- DjangoProjectA/app_a/models.py
+++ DjangoProjectA/app_a/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=32, verbose_name="作者的姓名")
+
+    def __str__(self):
+        return self.name
--- DjangoProjectA/app_a/views.py
+++ DjangoProjectA/app_a/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def first_view(request):
+    return HttpResponse("Hello, world. 第一个视图")
