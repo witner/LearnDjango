8756412081ac831b8a9c0baf444b83c5325f76bf
huangdongdong <huangdongdong@daojia-inc.com>
2019-12-10 17:01:46 +0800

    B-04 Django-第二部分（熟练掌握）-模型（Model）设计-定义模型类（练习） 定义模型类

---
+++ DjangoProjectA/app_b/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppBConfig(AppConfig):
+    name = 'app_b'
---
+++ DjangoProjectA/app_b/models.py
@@ -0,0 +1,92 @@
+from django.db import models
+
+
+# Create your models here.
+
+
+class Teacher(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=16, verbose_name="教师名称")
+    class_id = models.ManyToManyField(to='Class', db_column="class_id", db_table="t_teach2cls")
+
+    class Meta:
+        db_table = 't_teacher'  # 定义表名为t_teacher
+
+    def __str__(self):
+        # 默认显示类的name字段
+        return self.name
+
+
+class Grade(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=16, verbose_name="年级名称")
+
+    class Meta:
+        db_table = 't_grade'
+
+    def __str__(self):
+        # 默认显示类的name字段
+        return self.name
+
+
+class Class(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=16, verbose_name="编辑名称")
+    grade_id = models.ForeignKey(to='Grade', to_field='id', on_delete=models.CASCADE, db_column="grade_id")
+
+    class Meta:
+        db_table = 't_class'
+
+    def __str__(self):
+        # 默认显示类的name字段
+        return self.name
+
+
+class Student(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=16, verbose_name="学生名称")
+    GENDER_CHOICES = (
+        (0, u"女"),
+        (1, u"男"),
+    )
+    gender = models.CharField(max_length=4, verbose_name="性别", choices=GENDER_CHOICES)
+    class_id = models.ForeignKey(to='Class', to_field='id', on_delete=models.CASCADE, db_column="class_id")
+    course_id = models.ManyToManyField(to='Course', through='Score', through_fields=('student_id', 'course_id'),
+                                       db_column="course_id")
+
+    class Meta:
+        db_table = 't_student'
+
+    def __str__(self):
+        # 默认显示类的name字段
+        return self.name
+
+
+class Course(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=16, verbose_name="课程名称")
+    teacher_id = models.ForeignKey(to='Teacher', to_field='id', on_delete=models.CASCADE, db_column="teacher_id")
+
+    class Meta:
+        db_table = 't_course'
+
+    def __str__(self):
+        # 默认显示类的name字段
+        return self.name
+
+
+class Score(models.Model):
+    id = models.AutoField(primary_key=True)
+    student_id = models.ForeignKey(verbose_name='学生', to="Student", to_field='id', on_delete=models.CASCADE,
+                                   db_column="student_id")
+    course_id = models.ForeignKey(verbose_name='课程', to="Course", to_field='id', on_delete=models.CASCADE,
+                                  db_column="course_id")
+    score = models.IntegerField(verbose_name="分数", default=0)
+
+    class Meta:
+        db_table = 't_score'
+        unique_together = [('student_id', 'course_id'), ]
+
+    def __str__(self):
+        v = self.student_id.name + "---" + self.course_id.name
+        return v
